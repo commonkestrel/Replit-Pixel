365880b1c68e26e331cdcbb5dc36f31a2a7027f4
Jibble330 <jettb6@gmail.com>
2022-11-14 23:11:37 -0800

    Remove unnecessary functions

--- main.go
+++ main.go
@@ -1,80 +1,70 @@
 package main
 
 import (
-	"image"
-	"os"
-	"path"
-	"runtime"
+    "os"
+    "image"
 
-	_ "image/png"
+    _ "image/png"
 
-	"github.com/faiface/pixel"
-	"github.com/faiface/pixel/imdraw"
-	"github.com/faiface/pixel/pixelgl"
-	"golang.org/x/image/colornames"
+    "github.com/faiface/pixel"
+    "github.com/faiface/pixel/imdraw"
+    "github.com/faiface/pixel/pixelgl"
+    "golang.org/x/image/colornames"
 )
 
 const SCREENX, SCREENY = 960, 540
 
 var (
-	win     *pixelgl.Window
-	imd     *imdraw.IMDraw
+    win     *pixelgl.Window
+    imd     *imdraw.IMDraw
 )
 
 // used for loading icons and sprites
 func LoadPicture(path string) (pixel.Picture, error) {
-	// loads and decodes PNG
-	file, err := os.Open(path)
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-	img, _, err := image.Decode(file)
-	if err != nil {
-		panic(err)
-	}
-	// converts to Pixel picture
-	return pixel.PictureDataFromImage(img), nil
-}
-
-// returns the absolute path of a path relative to the file's parent directory
-func relative(relative string) string {
-	_, filepath, _, _ := runtime.Caller(0)
-	dir := path.Dir(filepath)
-	return path.Join(dir, relative)
+    // loads and decodes PNG
+    file, err := os.Open(path)
+    if err != nil {
+        panic(err)
+    }
+    defer file.Close()
+    img, _, err := image.Decode(file)
+    if err != nil {
+        panic(err)
+    }
+    // converts to Pixel picture
+    return pixel.PictureDataFromImage(img), nil
 }
 
 func run() {
-	iconpath := relative("icon.png")
-	icon, err := LoadPicture(iconpath)
-	if err != nil {
-		panic(err)
-	}
+    icon, err := LoadPicture("icon.png")
+    if err != nil {
+        panic(err)
+    }
 
-	cfg := pixelgl.WindowConfig{
-		Title:  "Go Pixel",
-		Bounds: pixel.R(0, 0, SCREENX, SCREENY),
-		Icon:  []pixel.Picture{icon},
-		VSync: true,
-	}
-	win, err = pixelgl.NewWindow(cfg)
-	if err != nil {
-		panic(err)
-	}
+    cfg := pixelgl.WindowConfig{
+        Title:  "Go Pixel",
+        Bounds: pixel.R(0, 0, SCREENX, SCREENY),
+        Icon:  []pixel.Picture{icon},
+        VSync: true,
+    }
+    win, err = pixelgl.NewWindow(cfg)
+    if err != nil {
+        panic(err)
+    }
 
-	imd = imdraw.New(nil)
+    imd = imdraw.New(nil)
 
-	for !win.Closed() {
-		imd.Clear()
+    for !win.Closed() {
+        imd.Clear()
 
-		// game loop here
+        // game loop here
 
-		win.Clear(colornames.Black)
-		imd.Draw(win)
-		win.Update()
-	}
+        win.Clear(colornames.Black)
+        imd.Draw(win)
+        win.Update()
+    }
 }
 
 func main() {
-	pixelgl.Run(run)
+    pixelgl.Run(run)
 }
